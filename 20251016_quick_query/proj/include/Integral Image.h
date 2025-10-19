adcd64a957073f0207cffff6efee7f9d7dff981e
jerzha40 <jerryzhang40@gmail.com>
2025-10-19 20:59:40 +0800

    Code comments

--- 20251016_quick_query/proj/include/Integral Image.h
+++ 20251016_quick_query/proj/include/Integral Image.h
@@ -7,6 +7,15 @@ USE GBK to open the chinese characters
     get_mean
     get_var
 20251017’≈ΩÚÓ£
+
+∫Ø ˝Ω‚ Õ£∫
+get_sum:
+        input a matrix M export a matrix data
+        the data[a,b]=sum_{i=0}^{a}(sum_{j=0}^{b}(M[i,j]))
+get_mean:
+        input the sum matrix SM start at [si,sj]
+        get a rectangle with Side length of [li+1,lj+1]
+        the width is determined the row width of SM
 */
 #ifndef _INTEGRAL_IMAGE_H_
 #define _INTEGRAL_IMAGE_H_
